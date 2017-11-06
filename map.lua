5501dc3af4a184ef65e271431a2f83bcc611c2af
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-11-06 16:17:37 +1300

    Make map drawn to canvas first during update so transformation is not mixed.

--- map.lua
+++ map.lua
@@ -12,8 +12,8 @@ function map:create()
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -24,6 +24,8 @@ function map:create()
     }
 
     object.tile_size = 16
+    object.w = #object.tiles[1] * object.tile_size
+    object.h = #object.tiles * object.tile_size
 
     love.graphics.setDefaultFilter("nearest")
 
@@ -32,19 +34,35 @@ function map:create()
         love.graphics.newImage("assets/tile_1.png"),
     }
 
+    object.canvas = love.graphics.newCanvas(object.w, object.h)
+
     return object
 end
 
 function map:update()
+    self:update_canvas()
+end
 
+function map:update_canvas()
+    love.graphics.setCanvas(self.canvas)
+        love.graphics.push()
+            for y, row in pairs(self.tiles) do
+                y = y - 1
+                for x, tile in pairs(row) do
+                    x = x - 1
+                    love.graphics.draw(self.tile_sprites[tile + 1], self.tile_size * x, self.tile_size * y)
+                end
+            end
+        love.graphics.pop()
+    love.graphics.setCanvas()
+end
+
+function map:get_origin()
+    return {self.w / 2, self.h / 2}
 end
 
 function map:draw()
-    for y, row in pairs(self.tiles) do
-        y = y - 1
-        for x, tile in pairs(row) do
-            x = x - 1
-            love.graphics.draw(self.tile_sprites[tile + 1], self.tile_size * x, self.tile_size * y)
-        end
-    end
+    love.graphics.push()
+        love.graphics.draw(self.canvas)
+    love.graphics.pop()
 end
--- world.lua
+++ world.lua
@@ -5,21 +5,34 @@ function world:create()
     setmetatable(object, {__index = self} )
 
     object.map = map:create()
-    object.entities = { entity:create(0, 0, 16, 16) }
+    object.entities = { entity:create(32, 32, 16, 16) }
+    object.canvas = love.graphics.newCanvas()
 
     return object
 end
 
 function world:update()
+    self.map:update()
+
     for i, entity in pairs(self.entities) do
         entity:update()
     end
+
+    self:update_canvas()
+end
+
+function world:update_canvas()
+    love.graphics.setCanvas(self.canvas)
+        love.graphics.clear()
+
+        self.map:draw()
+        for i, entity in pairs(self.entities) do
+            entity:draw()
+        end
+
+    love.graphics.setCanvas()
 end
 
 function world:draw()
-    self.map:draw()
-
-    for i, entity in pairs(self.entities) do
-        entity:draw()
-    end
+    love.graphics.draw(self.canvas)
 end
