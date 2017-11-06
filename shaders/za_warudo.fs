1e101e0a3735409520f18a4151be73e621a2a0af
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-11-07 02:44:56 +1300

    Za warudo now has invert color and better ripple.

--- shaders/za_warudo.fs
+++ shaders/za_warudo.fs
@@ -18,15 +18,36 @@ vec2 to_cartesian(vec2 origin, vec2 point)
     return vector + origin;
 }
 
+vec4 ripple(Image texture, vec2 texture_coords)
+{
+    number d = distance(center, texture_coords);
+    number factor = abs((t * 2 - d));
+
+    vec2 new_coords = to_polar(center, texture_coords);
+    new_coords[0] *= 1 + factor / 1.5;
+    new_coords = to_cartesian(center, new_coords);
+
+    return Texel(texture, new_coords) + factor * 4;
+}
+
+vec4 invert_color(vec4 pixel)
+{
+    return vec4(1 - pixel[0], 1 - pixel[1], 1 - pixel[2], pixel[3]);
+}
+
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
-    if (distance(center, texture_coords) < t) {
-        vec2 new_coords = to_polar(center, texture_coords);
-        new_coords[0] *= t;
-        new_coords = to_cartesian(center, new_coords);
+    vec4 pixel = Texel(texture, texture_coords);
 
-        return Texel(texture, new_coords);
+    number d = distance(center, texture_coords);
+
+    if (d < t * 2.2 && d > t * 1.8) {
+        pixel = ripple(texture, texture_coords);
     }
 
-    return Texel(texture, texture_coords);
+    if (d < t * 2.2) {
+        pixel = invert_color(pixel);
+    }
+
+    return pixel;
 }
